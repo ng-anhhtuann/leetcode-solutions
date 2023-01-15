7531eae6dc504a58cb5f4a118b2b2774e0ac152a
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-01-16 00:30:58 +0700

    Remove Nth Node From End Of A Node

---
+++ 19-Remove-Nth-Node-From-End.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        // Advances first pointer so that the gap between first and second is n nodes
+        // apart
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+        // Move first to the end, maintaining the gap
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
+    // public ListNode removeNthFromEnd(ListNode head, int n) {
+    // List<Integer> list = new ArrayList<Integer>();
+    // while ( head != null ){
+    // list.add(head.val);
+    // head = head.next;
+    // }
+    // int pos = list.size() - n;
+    // list.remove(pos);
+    // return createListNodeWithList(list);
+    // }
+    // public ListNode createListNodeWithList(List<Integer> list) {
+    // ListNode dummy = new ListNode(0);
+    // ListNode current = dummy;
+    // for (int val : list) {
+    // current.next = new ListNode(val);
+    // current = current.next;
+    // }
+    // return dummy.next;
+    // }
+}

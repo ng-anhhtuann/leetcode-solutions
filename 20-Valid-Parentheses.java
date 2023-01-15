f670c46fbe387b506d160aed2fafe281fbf0966d
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-01-16 00:32:39 +0700

    Valid Prantheses

---
+++ 20-Valid-Parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == ')' && top != '(') {
+                    return false;
+                }
+                if (c == '}' && top != '{') {
+                    return false;
+                }
+                if (c == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}

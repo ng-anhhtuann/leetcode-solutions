224bddabf24fea29ab7911a6d468411269d87c85
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-01-18 00:32:50 +0700

    Swap Nodes in pair

---
+++ 24-Swap-Nodes-In-Pair.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dum = new ListNode(0);
+        dum.next = head;
+        ListNode prev = dum;
+        while (head != null && head.next != null) {
+            ListNode first = head;
+            ListNode second = head.next;
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+            prev = first;
+            head = first.next;
+        }
+        return dum.next;
+    }
+}

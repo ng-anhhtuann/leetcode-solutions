557a39a2f6aeecd6855bb91e4f1fdb1f12af0458
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-01-20 11:52:31 +0700

    Squares of a sorted array

---
+++ 977-Squares-Of-A-Sorted-Array.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    // O(nlogn)
+    // public int[] sortedSquares(int[] a) {
+    // int n = a.length;
+    // int[] res = new int[n];
+    // for ( int i = 0; i < n; i++){
+    // res[i] = a[i]*a[i];
+    // }
+    // Arrays.sort(res);
+    // return res;
+    // }
+
+    // O(n)
+    // Compare abs of head vs tail, which greater then pass the square to the end
+    // and decrease tracking spot k
+    public int[] sortedSquares(int[] a) {
+        int n = a.length;
+        int[] res = new int[n];
+        int i = 0, j = n - 1;
+        int k = n - 1;
+        while (i <= j) {
+            if (Math.abs(a[i]) > Math.abs(a[j])) {
+                res[k--] = a[i] * a[i];
+                i++;
+            } else {
+                res[k--] = a[j] * a[j];
+                j--;
+            }
+        }
+        return res;
+    }
+}

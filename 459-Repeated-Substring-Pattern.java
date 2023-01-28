d77a528c14e7bf1d6baacc5c70df5ef308c1861d
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-01-28 08:59:27 +0700

    Repeated Substring Pattern

---
+++ 459-Repeated-Substring-Pattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                String substring = s.substring(0, i);
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < n / i; j++) {
+                    sb.append(substring);
+                }
+                if (s.equals(sb.toString())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // public boolean repeatedSubstringPattern(String s) {
+    // String str = s + s;
+    // return str.substring(1, str.length() - 1).contains(s);
+    // }
+
+}

647842bc201205fe7ed2f88e05d9edd4a77e00b4
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-01-31 12:44:28 +0700

    Reverse prefix of word.java

---
+++ 2000-Reverse-Prefix-Of-Word.java
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * Slow approach
+     */
+    // public String reversePrefix(String w, char a) {
+    // if ( !w.contains(String.valueOf(a))) return w;
+    // int idx = w.indexOf(String.valueOf(a));
+    // String substr = w.substring(0,idx+1);
+    // String tail = w.substring(idx+1,w.length());
+    // int len = substr.length();
+    // char[] head = substr.toCharArray();
+    // for ( int i = 0 ; i < len/2; i++ ){
+    // char tmp = head[i];
+    // head[i] = head[len-1-i];
+    // head[len-1-i] = tmp;
+    // }
+    // String newHead = new String(head);
+    // return newHead + "" + tail;
+    // }
+
+    /**
+     * Better approach using StringBuilder
+     */
+    public String reversePrefix(String w, char a) {
+        if (!w.contains(String.valueOf(a)))
+            return w;
+        int idx = w.indexOf(a);
+        int len = idx + 1;
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = len - 1; i >= 0; i--) {
+            sb.append(w.charAt(i));
+        }
+        return sb.append(w, len, w.length()).toString();
+    }
+
+}

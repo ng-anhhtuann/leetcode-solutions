cbb5680ffadf5aa660135baf0686638160532370
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-02-03 00:19:40 +0700

    Multiple strings

---
+++ 43-Multiple-String.java
@@ -0,0 +1,24 @@
+class Solution {
+    public static String multiply(String a, String b) {
+        int m = a.length();
+        int n = b.length();
+        int[] res = new int[m + n];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (a.charAt(i) - '0') * (b.charAt(j) - '0');
+                int p1 = i + j;
+                int p2 = i + j + 1;
+                int sum = mul + res[p2];
+                res[p1] += sum / 10;
+                res[p2] = sum % 10;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i : res) {
+            if (!(sb.length() == 0 && i == 0)) {
+                sb.append(i);
+            }
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}

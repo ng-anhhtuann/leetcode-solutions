34d0edf72d88e29f71bcdc3b1b6a3cc5f956249e
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-02-10 12:32:46 +0700

    Naming a company

---
+++ 2306-Naming-A-Company.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long distinctNames(String[] a) {
+        List<String> arr[] = new ArrayList[26];
+        long res = 0;
+        for (int i = 0; i < 26; i++) {
+            arr[i] = new ArrayList<>();
+        }
+        for (String s : a) {
+            arr[s.charAt(0) - 'a'].add(s.substring(1));
+        }
+        for (int i = 0; i < 25; i++) {
+            for (int j = i + 1; j < 26; j++) {
+                Set<String> set = new HashSet<>();
+                set.addAll(arr[i]);
+                set.addAll(arr[j]);
+                res += (arr[i].size() - set.size()) * (arr[j].size() - set.size());
+            }
+        }
+        return res * 2;
+    }
+}

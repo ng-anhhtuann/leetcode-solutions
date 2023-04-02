f5a1d58795fb4251bd2e78231e4e7aa5a463dac1
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-04-02 21:19:46 +0700

    Same tree

---
+++ 100-Same-Tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+ * Traverse each by recursion
+ */
+class Solution {
+    public boolean isSameTree(TreeNode a, TreeNode b) {
+        if (a == null && b == null)
+            return true;
+        if (a == null || b == null)
+            return false;
+        if (a.val != b.val)
+            return false;
+        return isSameTree(a.left, b.left) && isSameTree(a.right, b.right);
+    }
+}

b84f956cf346a6188dfe8f3d53ab338c8368618e
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2023-05-17 16:45:11 +0700

    Bitwise and number range

--- 201-Bitwise-And-Numbers-Range.java
+++ 201-Bitwise-And-Numbers-Range.java
@@ -1,10 +1,8 @@
 class Solution {
     public static int rangeBitwiseAnd(int a, int b) {
-        int res = 0;
         int m = Integer.MAX_VALUE;
         while ((a & m) != (b & m)) {
             m <<= 1;
-            res <<= 1;
         }
         return a & m;
     }

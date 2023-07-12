e3c506dd6ba9646faf6c5018c181c86c00614490
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2023-07-13 00:01:24 +0700

    Ugly number

---
+++ 263-Ugly-Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if ( n == 1 ) return true;
+        if ( n == 0 ) return false;
+        while ( n != 1 & (n % 2 == 0 || n % 5 == 0 || n % 3 == 0)) {
+            if ( n % 2 == 0) n /= 2;
+            if ( n % 3 == 0) n /= 3;
+            if ( n % 5 == 0) n /= 5;
+        }
+        return n == 1;
+    }
+}

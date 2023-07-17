748a2ecf1b6004104def4bee0fd742bf08e32af4
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2023-07-17 23:37:25 +0700

    Impl queue using stacks

---
+++ 232-Impl-Queue-Using-Stacks.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+class MyQueue {
+    private Stack<Integer> inStack;
+    private Stack<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new Stack<Integer>();
+        outStack = new Stack<Integer>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.pop();
+    }
+
+    public int peek() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+}

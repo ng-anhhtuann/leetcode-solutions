67a51e82cdb799a3b1d80a19d582e70164b06399
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2023-09-16 09:28:35 +0700

    First unique character in a tring

---
+++ 387-First-Unique-Character-In-A-String.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        for ( int i = 0 ; i < s.length(); i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return s.indexOf(String.valueOf(entry.getKey()));
+            }
+        }
+        return -1;
+    }
+}

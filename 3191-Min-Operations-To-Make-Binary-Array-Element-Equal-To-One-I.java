7844cbc1c4a426c2fa72c117d27d4fee3dacb4b1
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2024-06-29 22:24:14 +0700

    Find min operators to make binary array element equal to one 1

---
+++ 3191-Min-Operations-To-Make-Binary-Array-Element-Equal-To-One-I.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int operations = 0;
+
+        for (int i = 0; i <= n - 3; i++) {
+            if (nums[i] == 0) {
+                flip(nums, i);
+                operations++;
+            }
+        }
+
+        for (int num : nums) {
+            if (num == 0) {
+                return -1;
+            }
+        }
+
+        return operations;
+    }
+
+    private void flip(int[] nums, int start) {
+        for (int i = start; i < start + 3; i++) {
+            nums[i] = 1 - nums[i];
+        }
+    }
+}

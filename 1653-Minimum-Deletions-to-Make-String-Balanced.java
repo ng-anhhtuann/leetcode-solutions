8f0deab41026cac7b844591b31d734bdfce823d3
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2024-08-16 14:25:53 +0700

    Minimum deletions to make string balanced

---
+++ 1653-Minimum-Deletions-to-Make-String-Balanced.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int n = s.length();
+        int count = 0;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0 ; i < n; i++) {
+            if (!stack.empty() && s.charAt(i) == 'a' && stack.peek() == 'b') {
+                stack.pop();
+                count++;
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+        return count;
+    }
+}

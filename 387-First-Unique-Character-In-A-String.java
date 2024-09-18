ea9071506beb4b1b4364ced0bbd909be3f55b234
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2024-09-19 00:14:38 +0700

    First unique character in a string(faster)

--- 387-First-Unique-Character-In-A-String.java
+++ 387-First-Unique-Character-In-A-String.java
@@ -1,5 +1,16 @@
 class Solution {
     public int firstUniqChar(String s) {
+        char[] c = s.toCharArray();
+        int[] a = new int[26];
+        for (char i : c)
+            a[i - 'a']++;
+        for (int i = 0; i < c.length; i++)
+            if (a[c[i] - 'a'] == 1)
+                return i;
+        return -1;
+    }
+
+    public int firstUniqChar(String s) {
         LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
         for ( int i = 0 ; i < s.length(); i++){
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);

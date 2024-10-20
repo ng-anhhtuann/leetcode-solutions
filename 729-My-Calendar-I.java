593c446539e73e907b0ee02490592f9e58a844ff
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2024-10-20 21:15:25 +0700

    My calendar I

---
+++ 729-My-Calendar-I.java
@@ -0,0 +1,25 @@
+class MyCalendar {
+    List<int[]> cal;
+    public MyCalendar() {
+        cal = new ArrayList<>();
+    }
+
+    public boolean book(int start, int end) {
+        for ( int[] i : cal ) {
+            if (i[0] == start) return false;
+            if (i[0] <= start && i[1] > start) return false;
+            if (i[0] < end && i[1] >= end) return false;
+            if (i[0] >= start && i[1] <= end) return false;
+        }
+        cal.add(new int[]{start, end});
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
+
+// 19-30 33-51 53-63 75-92 97-100

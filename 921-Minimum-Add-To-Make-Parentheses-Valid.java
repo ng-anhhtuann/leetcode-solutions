7439473db06884f35dbb848a95c2ef421509992c
morphibius <nguyenanhtuan1232@gmail.com>
2024-11-12 14:24:25 +0700

    Minimum add to make parenteses valid

--- 921-Minimum-Add-To-Make-Parentheses-Valid.java
+++ 921-Minimum-Add-To-Make-Parentheses-Valid.java
@@ -1,17 +1,32 @@
 class Solution {
-    public int minAddToMakeValid(String s) {
-        Stack<Character> stack = new Stack<>();
+    // public int minAddToMakeValid(String s) {
+    // Stack<Character> stack = new Stack<>();
 
+    // char[] cs = s.toCharArray();
+    // int count = 0;
+    // for (char c : cs) {
+    // if (c == '(')
+    // stack.push(c);
+    // else if (stack.isEmpty() || stack.pop() == ')')
+    // count++;
+    // }
+    // return count + stack.size();
+    // }
+
+    public int minAddToMakeValid(String s) {
         char[] cs = s.toCharArray();
-        int count = 0;
+        int open = 0;
+        int close = 0;
         for (char c : cs) {
             if (c == '(')
-                stack.push(c);
-            if (c == ')') {
-                if (stack.isEmpty() || stack.pop() == ')')
-                    count++;
+                open++;
+            else {
+                if (open > 0)
+                    open--;
+                else
+                    close++;
             }
         }
-        return count + stack.size();
+        return open + close;
     }
 }

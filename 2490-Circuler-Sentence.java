e09b8f2082ea2855de5ca8f6b7118b1a5b299a8c
morphibius <nguyenanhtuan1232@gmail.com>
2024-12-10 14:24:25 +0700

    Circular sentence

--- 2490-Circuler-Sentence.java
+++ 2490-Circuler-Sentence.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean isCircularSentence(String str) {
+        if (str.charAt(0) != str.charAt(str.length() - 1))
+            return false;
         String[] listStr = str.split(" ");
         int n = listStr.length;
         for (int i = 1; i < n; i++)
             if (listStr[i].charAt(0) != listStr[i - 1].charAt(listStr[i - 1].length() - 1))
                 return false;
-        return str.charAt(0) == str.charAt(str.length() - 1);
+        return true;
     }
 }

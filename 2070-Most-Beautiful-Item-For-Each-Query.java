798e1ac082a589891a5c3c19914e06385e1d003a
morphibius <nguyenanhtuan1232@gmail.com>
2025-01-14 14:24:25 +0700

    Most beautiful item for each query

--- 2070-Most-Beautiful-Item-For-Each-Query.java
+++ 2070-Most-Beautiful-Item-For-Each-Query.java
@@ -1,16 +1,34 @@
 class Solution {
+
     public int[] maximumBeauty(int[][] range, int[] q) {
-        int n = range.length;
-        int m = q.length;
-        int[] res = new int[m];
-        for (int i = 0; i < m; i++) {
-            int max = 0;
-            for (int[] tmp : range) {
-                if (q[i] >= tmp[0])
-                    max = Math.max(max, tmp[1]);
-            }
-            res[i] = max;
+        int[] ans = new int[q.length];
+
+        Arrays.sort(range, (a, b) -> a[0] - b[0]);
+        int max = range[0][1];
+        for (int i = 0; i < range.length; i++) {
+            max = Math.max(max, range[i][1]);
+            range[i][1] = max;
         }
-        return res;
+
+        for (int i = 0; i < q.length; i++)
+            ans[i] = find(range, q[i]);
+
+        return ans;
+    }
+
+    private int find(int[][] range, int t) {
+        int l = 0;
+        int r = range.length - 1;
+        int max = 0;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (range[mid][0] > t) {
+                r = mid - 1;
+            } else {
+                max = Math.max(max, range[mid][1]);
+                l = mid + 1;
+            }
+        }
+        return max;
     }
 }

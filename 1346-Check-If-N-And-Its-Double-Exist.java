b9529afb0e8439608cb24f85b7aaea448ca5e839
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2025-03-10 20:25:36 +0700

    Check if n and its double exist

--- 1346-Check-If-N-And-Its-Double-Exist.java
+++ 1346-Check-If-N-And-Its-Double-Exist.java
@@ -1,16 +1,16 @@
 class Solution {
-    public boolean checkIfExist(int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i : arr)
-            map.put(i, map.getOrDefault(i, 0) + 1);
-        for (int i : arr) {
-            if (i != 0 && map.containsKey(2 * i))
-                return true;
-            if (i == 0 && map.get(i) > 1)
-                return true;
-        }
-        return false;
-    }
+    // public boolean checkIfExist(int[] arr) {
+    // Map<Integer, Integer> map = new HashMap<>();
+    // for (int i : arr)
+    // map.put(i, map.getOrDefault(i, 0) + 1);
+    // for (int i : arr) {
+    // if (i != 0 && map.containsKey(2 * i))
+    // return true;
+    // if (i == 0 && map.get(i) > 1)
+    // return true;
+    // }
+    // return false;
+    // }
 
     public boolean checkIfExist(int[] arr) {
         Set<Integer> set = new HashSet<>();

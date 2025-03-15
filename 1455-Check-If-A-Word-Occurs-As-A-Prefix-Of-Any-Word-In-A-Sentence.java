b0512a847bdd275fa083accca13dff54245b72e8
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2025-03-15 20:25:36 +0700

    Check if a word occurs as a prefix of any word in a sentence

--- 1455-Check-If-A-Word-Occurs-As-A-Prefix-Of-Any-Word-In-A-Sentence.java
+++ 1455-Check-If-A-Word-Occurs-As-A-Prefix-Of-Any-Word-In-A-Sentence.java
@@ -1,11 +1,38 @@
 class Solution {
     public int isPrefixOfWord(String s, String key) {
-        String[] str = s.split(" ");
-        for (int i = 0; i < str.length; i++) {
-            if (str[i].startsWith(key))
-                return i + 1;
+        int wordIndex = 1;
+        int start = 0;
+
+        for (int i = 0; i <= s.length(); i++) {
+            if (i == s.length() || s.charAt(i) == ' ') {
+                String word = extractSubstring(s, start, i);
+                if (isPrefix(word, key)) {
+                    return wordIndex;
+                }
+                wordIndex++;
+                start = i + 1;
+            }
         }
         return -1;
     }
 
+    private String extractSubstring(String s, int start, int end) {
+        char[] chars = new char[end - start];
+        for (int i = start; i < end; i++) {
+            chars[i - start] = s.charAt(i);
+        }
+        return new String(chars);
+    }
+
+    private boolean isPrefix(String str, String prefix) {
+        if (prefix.length() > str.length()) {
+            return false;
+        }
+        for (int i = 0; i < prefix.length(); i++) {
+            if (str.charAt(i) != prefix.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }

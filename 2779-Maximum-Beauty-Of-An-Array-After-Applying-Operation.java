02e6c4e7486d373eda60c35b9555513b520d8009
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2025-03-29 20:25:36 +0700

    Maximum beauty of an array after applying operation

--- 2779-Maximum-Beauty-Of-An-Array-After-Applying-Operation.java
+++ 2779-Maximum-Beauty-Of-An-Array-After-Applying-Operation.java
@@ -1,21 +1,16 @@
 class Solution {
+
     public int maximumBeauty(int[] a, int k) {
-        int n = a.length;
-        int max = 0;
-        for (int i : a)
-            max = Math.max(max, i + k);
-        int[] dp = new int[max + 1];
-        for (int i : a) {
-            int start = Integer.MAX_VALUE;
-            int end = Integer.MIN_VALUE;
-            start = (i - k < 0) ? 0 : i - k;
-            end = i + k;
-            for (int j = start; j <= end; j++)
-                dp[j]++;
+        Arrays.sort(a);
+        int r = 0;
+        int mx = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            while (r < a.length && a[r] - a[i] <= 2 * k) {
+                r++;
+            }
+            mx = Math.max(mx, r - i);
         }
-        max = 0;
-        for (int i : dp)
-            max = Math.max(i, max);
-        return max;
+        return mx;
     }
 }

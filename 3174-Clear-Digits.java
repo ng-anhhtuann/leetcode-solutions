dff70f10c9d29471e2e66086208d0cf07ffbb90e
ng-anhhtuann <nguyenanhtuan1232@gmail.com>
2025-04-09 20:25:36 +0700

    Clear digits

--- 3174-Clear-Digits.java
+++ 3174-Clear-Digits.java
@@ -1,35 +1,19 @@
 class Solution {
     public String clearDigits(String s) {
-
-        StringBuilder sb = new StringBuilder();
-        char[] c = s.toCharArray();
-
-        Stack<Character> st = new Stack<>();
-        Queue<Character> q = new LinkedList<>();
-
         int count = 0;
-        for (char i : c) {
-            q.add(i);
-            count += Character.isDigit(i) ? 1 : 0;
-        }
-        if (count == 0)
-            return s;
-        while (count > 0) {
-            while (q.peek() != null && !Character.isDigit(q.peek()))
-                st.push(q.poll());
+        StringBuilder sb = new StringBuilder(s);
+        while (count < sb.length()) {
+            if (Character.isDigit(sb.charAt(count))) {
+                sb.deleteCharAt(count);
+                if (count > 0) {
+                    sb.deleteCharAt(count - 1);
+                    count--;
+                }
 
-            while (q.peek() != null && Character.isDigit(q.peek())) {
-                q.remove();
-                count--;
-                if (!st.empty())
-                    st.pop();
+            } else {
+                count++;
             }
         }
-        while (!q.isEmpty())
-            sb.append(q.poll());
-        while (!st.empty())
-            sb.insert(0, st.pop());
-
         return sb.toString();
     }
 }

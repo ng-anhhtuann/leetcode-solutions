8cd0c3433644ea67b466c9f9e639dbb77f614532
morphibius <nguyenanhtuan1232@gmail.com>
2025-05-08 14:25:35 +0700

    Delete Nodes From Linked List Present in Array

--- 3217-Delete-Nodes-From-Linked-List-Present-In-Array.java
+++ 3217-Delete-Nodes-From-Linked-List-Present-In-Array.java
@@ -9,17 +9,21 @@
  * }
  */
 class Solution {
-    public ListNode modifiedList(int[] nums, ListNode root) {
+    public ListNode modifiedList(int[] a, ListNode root) {
         int max = 0;
-        for (int i : nums) max = Math.max(max, i);
+        for (int i : a)
+            max = Math.max(max, i);
 
         boolean[] v = new boolean[max + 1];
 
-        for (int i : nums) v[i] = true;
+        for (int i : a)
+            v[i] = true;
 
-        while (root != null && root.val <= max && v[root.val] == true) root = root.next;
+        while (root != null && root.val <= max && v[root.val] == true)
+            root = root.next;
 
-        if (root == null) return root;
+        if (root == null)
+            return root;
 
         ListNode ptr = root;
 
@@ -32,28 +36,28 @@ class Solution {
         return root;
     }
 
-/** Slow solution 21 ms*/
-    // public ListNode modifiedList(int[] nums, ListNode root) {
-    //     Map<Integer, Integer> map = new HashMap<>();
-    //     int n = nums.length;
-    //     for (int i : nums)
-    //         map.put(i, 1);
+    /** Slow solution 21 ms */
+    // public ListNode modifiedList(int[] a, ListNode root) {
+    // Map<Integer, Integer> map = new HashMap<>();
+    // int n = a.length;
+    // for (int i : a)
+    // map.put(i, 1);
 
-    //     ListNode node = new ListNode();
-    //     ListNode dum = node;
-    //     while (root != null) {
-    //         if (map.get(root.val) == null) {
-    //             if (dum == null) {
-    //                 dum = new ListNode(root.val);
-    //             } else {
-    //                 ListNode curr = new ListNode(root.val);
-    //                 dum.next = curr;
-    //                 dum = dum.next;
-    //             }
-    //         }
-    //         root = root.next;
-    //     }
-    //     dum.next = null;
-    //     return node.next;
+    // ListNode node = new ListNode();
+    // ListNode dum = node;
+    // while (root != null) {
+    // if (map.get(root.val) == null) {
+    // if (dum == null) {
+    // dum = new ListNode(root.val);
+    // } else {
+    // ListNode curr = new ListNode(root.val);
+    // dum.next = curr;
+    // dum = dum.next;
+    // }
+    // }
+    // root = root.next;
+    // }
+    // dum.next = null;
+    // return node.next;
     // }
 }

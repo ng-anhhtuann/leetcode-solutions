0be062f232376ebb13328b4d10d57bfe74ad368d
morphibius <nguyenanhtuan1232@gmail.com>
2025-07-17 14:25:35 +0700

    Number of Zero-Filled Subarrays

--- 2348-Number-Of-Zero-Filled-Subarrays.java
+++ 2348-Number-Of-Zero-Filled-Subarrays.java
@@ -1,17 +1,32 @@
+// class Solution {
+//     public long zeroFilledSubarray(int[] a) {
+//         long res = 0;
+//         int n = a.length;
+//         long count = 0;
+//         for ( int i = 0 ; i < n; i++){
+//             if ( a[i] == 0 ) count++;
+//             if ( a[i] != 0 || i == n-1){
+//                 if ( count != 0){
+//                     res += count * (count+1) / 2;
+//                     count = 0;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// }
 class Solution {
     public long zeroFilledSubarray(int[] a) {
-        long res = 0;
-        int n = a.length;
         long count = 0;
-        for ( int i = 0 ; i < n; i++){
-            if ( a[i] == 0 ) count++;
-            if ( a[i] != 0 || i == n-1){
-                if ( count != 0){
-                    res += count * (count+1) / 2;
-                    count = 0;
-                }
+        int h = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 0) {
+                h++;
+                count += h;
+            } else {
+                h = 0;
             }
         }
-        return res;
+        return count;
     }
 }

f721dc8db60e77b0666d387f56c997ca29eb9bd7
morphibius <nguyenanhtuan1232@gmail.com>
2025-08-02 14:25:35 +0700

    Power of two

--- 231-Power-Of-Two.java
+++ 231-Power-Of-Two.java
@@ -1,16 +1,7 @@
 class Solution {
-    /** 1107 / 1109 test case */
-    // public boolean isPowerOfTwo(int n) {
-    //     double logRes = Math.log(n) / Math.log(2);
-    //     return Math.ceil(logRes) == Math.floor(logRes);
-    // }
-
-    /**AC */
-    public boolean isPowerOfTwo(int n){
-        if (n <= 0) {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0)
             return false;
-        }
         return (n & (n - 1)) == 0;
     }
-
 }

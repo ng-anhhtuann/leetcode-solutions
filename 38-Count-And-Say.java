28958c571d154bf9d2354fc9c04dabb55212ea70
morphibius <nguyenanhtuan1232@gmail.com>
2025-12-06 14:25:35 +0700

    Count and Say

--- 38-Count-And-Say.java
+++ 38-Count-And-Say.java
@@ -2,20 +2,20 @@ class Solution {
     public String countAndSay(int n) {
         String res = "1";
         for (int i = 1; i < n; i++) {
-            StringBuilder temp = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
             int count = 1;
             for (int j = 1; j < res.length(); j++) {
                 if (res.charAt(j) == res.charAt(j - 1)) {
                     count++;
                 } else {
-                    temp.append(count);
-                    temp.append(res.charAt(j - 1));
+                    sb.append(count);
+                    sb.append(res.charAt(j - 1));
                     count = 1;
                 }
             }
-            temp.append(count);
-            temp.append(res.charAt(res.length() - 1));
-            res = temp.toString();
+            sb.append(count);
+            sb.append(res.charAt(res.length() - 1));
+            res = sb.toString();
         }
         return res;
     }
